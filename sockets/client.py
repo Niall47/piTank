d56f77dd90ad9a77521d34e38701d75425215434
Niall <niall47s@gmail.com>
2021-09-05 15:39:26 +0100

    Server can send keys to client. very nice

--- sockets/client.py
+++ sockets/client.py
@@ -1,16 +1,16 @@
+
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+def client():
+    host = socket.gethostname()
+    port = 5000
 
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
 
-while True:
-    full_msg = ''
     while True:
-        msg = s.recv(8)
-        if len(msg) <= 0:
-            break
-        full_msg += msg.decode("utf-8")
+        data = client_socket.recv(1024).decode()
+        print(data)
 
-    if len(full_msg) > 0:
-        print(full_msg)
+if __name__ == '__main__':
+    client()
--- sockets/server.py
+++ sockets/server.py
@@ -1,12 +1,43 @@
 import socket
+import time
+import pyxhook
+import os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
 
-while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!!","utf-8"))
-    clientsocket.close()
+def send_inputs():
+    global connection
+
+    new_hook = pyxhook.HookManager()
+    new_hook.KeyDown = OnKeyPress
+    new_hook.KeyUp = OnKeyRelease
+    host = socket.gethostname()
+    port = 5000
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(2)
+
+    while True:
+        conn, address = server_socket.accept()
+        connection = conn
+        print("Connection from: " + str(address))
+        new_hook.HookKeyboard()
+        try:
+            new_hook.start()
+        except KeyboardInterrupt:
+            # User cancelled
+            pass
+        except Exception as ex:
+            print(ex)
+        # conn.send('helloworld'.encode())  # send data to the client
+
+    # conn.close()  # close the connection
+def OnKeyPress(event):
+    input = '{}\n'.format(event.Key)
+    connection.send(input.encode())
+
+def OnKeyRelease(event):
+    connection.send('-'.encode())
+
+if __name__ == '__main__':
+    connection = ''
+    send_inputs()
