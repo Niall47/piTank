11bf32f85bb4b4270bfdad27cf6377f9324fcf1e
Niall <niall47s@gmail.com>
2021-09-05 12:07:58 +0100

    adding led

--- tank.py
+++ tank.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 import os
 import pygame
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
 screen = pygame.display.set_mode([240, 160])
 
@@ -72,6 +74,9 @@ def shutdown():
     GPIO.output(15,False)
     GPIO.cleanup()
 
+GPIO.output(16, True)
+time.sleep(5)
+GPIO.output(16, False)
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.KEYDOWN:
