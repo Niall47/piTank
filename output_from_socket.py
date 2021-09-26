aaae250a609e5df8660700e9f4a304c76e653923
Niall <niall47s@gmail.com>
2021-09-26 19:45:00 +0100

    printing server commads

---
+++ output_from_socket.py
@@ -0,0 +1,11 @@
+def client():
+    print('Connecting to server')
+    host = '192.168.0.47'
+    port = 5000
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+
+
+    while True:
+        data = client_socket.recv(1024).decode()
+        print(data)
