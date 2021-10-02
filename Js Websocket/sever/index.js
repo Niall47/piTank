f3b39e6f73dc79f674f911f7487c1d061fb7d7d8
Niall <niall47s@gmail.com>
2021-10-02 15:15:25 +0100

    sending messages

--- Js Websocket/sever/index.js
+++ Js Websocket/sever/index.js
@@ -1,11 +1,15 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({port: 8080});
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", ws => {
-    console.log("New client connected")
+    console.log("New client connected");
+
+    ws.on("message", data => {
+        console.log(`Client has sent: ${data}`);
+    });
 
     ws.on("close", () => {
         console.log("Client has disconnected");
-    })
+    });
 });
