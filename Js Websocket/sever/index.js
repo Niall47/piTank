bd74a58f0a1501523d442011d4d4f0ef6dfbfd74
Niall <niall47s@gmail.com>
2021-10-02 16:32:15 +0100

    translate inputs in switch statement

--- Js Websocket/sever/index.js
+++ Js Websocket/sever/index.js
@@ -6,7 +6,25 @@ wss.on("connection", ws => {
     console.log("New client connected");
 
     ws.on("message", data => {
+        let input = `${data}`;
         console.log(`Client has sent: ${data}`);
+
+        switch( input ) {
+            case 'Forward':
+                console.log('GPIO forward');
+                break;
+            case 'Back':
+                console.log('GPIO back');
+                break;
+            case 'Left':
+                console.log('GPIO Left');
+                break;
+            case 'Right':
+                console.log('GPIO Right');
+                break;
+            default:
+                console.log(input);
+        }
     });
 
     ws.on("close", () => {
