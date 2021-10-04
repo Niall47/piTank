bcf41119d01468d04d5a315b98beeb02392ca852
Niall <niall47s@gmail.com>
2021-10-04 14:15:41 +0100

    testing dc motors

--- Js Websocket/index.js
+++ Js Websocket/index.js
@@ -1,7 +1,11 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
 var Gpio = require('onoff').Gpio;
-var LED = new Gpio(23, 'out');
+var LED = new Gpio(23, 'out'),
+DC4 = new Gpio(4, 'out'),
+DC17 = new Gpio(17, 'out'),
+DC27 = new Gpio(27, 'out'),
+DC22 = new Gpio(22, 'out');
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8081 });
 
@@ -9,6 +13,7 @@ const wss = new WebSocket.Server({ port: 8081 });
 function shellCommand(command="", callback){
     exec(command, function(error, stdout, stderr){ callback(stdout); });
 }
+
 
 wss.on("connection", ws => {
     console.log("New client connected");
@@ -19,9 +24,11 @@ wss.on("connection", ws => {
         switch( input ) {
             case 'C':
                 console.log("Idle");
+                idle();
                 break;
             case 'N':
                 console.log('Forward');
+                forward();
                 break;
             case 'NE':
                 console.log('Forward/Right');
@@ -59,7 +66,20 @@ wss.on("connection", ws => {
 
     ws.on("close", () => {
         console.log("Client has disconnected");
-        LED.writeSync(1);
+        LED.writeSync(0);
     });
 
 });
+
+function idle (){
+    DC4.writeSync(0);
+    DC17.writeSync(0);
+    DC22.writeSync(0);
+    DC27.writeSync(0);
+}
+function forward() {
+    DC4.writeSync(1);
+    DC17.writeSync(1);
+    DC27.writeSync(0);
+    DC22.writeSync(1);
+};
