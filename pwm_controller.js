c895ca3043ca73567714fa2a4d0f9123c981f85c
Niall <niall.james@dvla.gov.uk>
2021-12-01 19:37:02 +0000

    setup to test with L298N

--- pwm_controller.js
+++ pwm_controller.js
@@ -1,175 +1,34 @@
-var sys = require('sys');
-var exec = require('child_process').exec;
-// var Gpio = require('onoff').Gpio;
-// var LED = new Gpio(23, 'out'),
-//     DC4 = new Gpio(4, 'out'),
-//     DC17 = new Gpio(17, 'out'),
-//     DC27 = new Gpio(27, 'out'),
-//     DC22 = new Gpio(22, 'out');
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8081 });
-
-
+const timer = ms => new Promise( res => setTimeout(res, ms));
 const Gpio = require('pigpio').Gpio;
+const left_pos = new Gpio(13, {mode: Gpio.OUTPUT});
+const left_neg = new Gpio(19, {mode: Gpio.OUTPUT});
+const right_pos = new Gpio(18, {mode: Gpio.OUTPUT});
+const right_neg = new Gpio(12, {mode: Gpio.OUTPUT});
+let interval = 1000;
 
-const led = new Gpio(13, {mode: Gpio.OUTPUT});
+(async function(){
+  console.log("left pos - 13")
+  left_pos.pwmWrite(255)
+  await timer(interval);
+  left_pos.pwmWrite(0)
+  console.log("left neg - 19")
+  left_neg.pwmWrite(255)
+  await timer(interval);
+  left_neg.pwmWrite(0)
 
-let dutyCycle = 0;
-
-setInterval(() => {
-  led.pwmWrite(dutyCycle);
-
-  dutyCycle += 5;
-  if (dutyCycle > 255) {
-    dutyCycle = 0;
-  }
-}, 20);
-
-
-
-// Pull apart hank the tank, check which pinouts support the pwm and rewire accordingly
-// update the dns settings for death-trap.uk to point to the hankthetank s3 bucket
-// update the index.html to also send the distance from center the joystick is at
-// now maybe a good time to address if our methoed of updating data is going to pass a stress test
-// how do other people do it using sockets?
-
-
-
-// Create shutdown function
-// function shellCommand(command="", callback){
-//     LED.writeSync(0); 
-//     exec(command, function(error, stdout, stderr){ callback(stdout); });
-// }
+  console.log("right pos - 18")
+  right_pos.pwmWrite(255)
+  await timer(interval);
+  right_pos.pwmWrite(0)
+  console.log("right neg - 12")
+  right_neg.pwmWrite(255)
+  await timer(interval);
+  right_neg.pwmWrite(0)
+  console.log("done")
+})()
 
 
-// wss.on("connection", ws => {
-//     console.log("New client connected");
-//     LED.writeSync(1);
-
-//     ws.on("message", data => {
-//         let input = `${data}`;
-//         switch( input ) {
-//             case 'C':
-//                 console.log("Idle");
-//                 idle();
-//                 break;
-//             case 'N':
-//                 console.log('Forward');
-//                 forward();
-//                 break;
-//             case 'NE':
-//                 console.log('Forward/Right');
-//                 forwardRight();
-//                 break;
-//             case 'E':
-//                 console.log('Right');
-//                 right();
-//                 break;
-//             case 'SE':
-//                 console.log('Backward/Right');
-//                 backwardRight();
-//                 break;
-//             case 'S':
-//                 console.log('Backward');
-//                 backward();
-//                 break;
-//             case 'SW':
-//                 console.log('Backward/Left');
-//                 backwardLeft();
-//                 break;
-//             case 'W':
-//                 console.log('Left');
-//                 left();
-//                 break;
-//             case 'NW':
-//                 console.log('Forward/Left');
-//                 forwardLeft();
-//                 break;
-//             case 'X':
-//                 console.log('Shutdown');
-//                 shellCommand(command="shutdown now");
-//                 break;
-//             case 'R':
-//                 console.log('Restart');
-//                 shellCommand(command="shutdown -r now");
-//                 break;
-//             default:
-//                 console.log(input);
-//         }
-//     });
-
-//     ws.on("close", () => {
-//         console.log("Client has disconnected");
-//         LED.writeSync(0);
-//     });
-
-// });
-
-// function idle (){
-//     DC4.writeSync(0);
-//     DC17.writeSync(0);
-//     DC22.writeSync(0);
-//     DC27.writeSync(0);
-// }
-
-// function forward() {
-//     DC4.writeSync(1);
-//     DC17.writeSync(0);
-//     DC27.writeSync(1);
-//     DC22.writeSync(1);
-// };
-
-// function forwardRight() {
-//     DC4.writeSync(1);
-//     DC17.writeSync(0);
-//     DC27.writeSync(0);
-//     DC22.writeSync(0);
-// };
-
-// function right() {
-//     DC4.writeSync(1);
-//     DC17.writeSync(0);
-//     DC27.writeSync(1);
-//     DC22.writeSync(0);
-// };
-
-// function backwardRight() {
-//     DC4.writeSync(1);
-//     DC17.writeSync(17);
-//     DC27.writeSync(0);
-//     DC22.writeSync(0);
-// };
-
-// function backward() {
-//     DC4.writeSync(1);
-//     DC17.writeSync(1);
-//     DC27.writeSync(1);
-//     DC22.writeSync(0);
-// };
-
-// function backwardLeft() {
-//     DC4.writeSync(0);
-//     DC17.writeSync(0);
-//     DC27.writeSync(1);
-//     DC22.writeSync(1);
-// };
-
-// function left() {
-//     DC4.writeSync(1);
-//     DC17.writeSync(1);
-//     DC27.writeSync(1);
-//     DC22.writeSync(1);
-// };
-
-// function forwardLeft() {
-//     DC4.writeSync(0);
-//     DC17.writeSync(0);
-//     DC27.writeSync(1);
-//     DC22.writeSync(0);
-// };
-
-// DC22 & DC27 Right Forward?
-// DC4 left Forward
-// DC17 Nothing?
-// DC27 Right backward
-// DC4 & DC17 Left backward
+left_pos.pwmWrite(0)
+left_neg.pwmWrite(0)
+right_pos.pwmWrite(0)
+right_neg.pwmWrite(0)
