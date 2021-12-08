8c39f13965a1ed75639055efa4dbef043f78d5c2
Niall <niall47s@gmail.com>
2021-12-08 22:02:10 +0000

    added gpio integration for test on pi

--- pwm_sliders.js
+++ pwm_sliders.js
@@ -1,34 +1,38 @@
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8081 });
-// const Gpio = require('pigpio').Gpio;
-// const pin13gpio = new Gpio(13, {mode: Gpio.OUTPUT});
-// const pin19gpio = new Gpio(19, {mode: Gpio.OUTPUT});
-// const pin18gpio = new Gpio(18, {mode: Gpio.OUTPUT});
-// const pin12gpio = new Gpio(12, {mode: Gpio.OUTPUT});
+const Gpio = require('pigpio').Gpio;
+const pin13gpio = new Gpio(13, {mode: Gpio.OUTPUT});
+const pin19gpio = new Gpio(19, {mode: Gpio.OUTPUT});
+const pin18gpio = new Gpio(18, {mode: Gpio.OUTPUT});
+const pin12gpio = new Gpio(12, {mode: Gpio.OUTPUT});
 
 wss.on("connection", ws => {
     console.log("New client connected");
-    // LED.writeSync(1);
 
     ws.on("message", data => {
-        let input = JSON.parse(`${data}`);
-        console.log(input)
-        switch( input ) {
-            case 'C':
-                console.log("Idle");
-
+        let input = JSON.parse(data);
+        console.log(input.pin)
+        switch( parseInt(input.pin )) {
+            case 13:
+                pin13gpio.pwmWrite(input.value);
                 break;
-            case 'N':
-                console.log('Forward');
-
+            case 19:
+                pin19gpio.pwmWrite(input.value);
+                break;
+            case 18:
+                pin18gpio.pwmWrite(input.value);
+                break;
+            case 12:
+                pin12gpio.pwmWrite(input.value);
+                break;
             default:
+                console.log('Unexpected input')
                 console.log(input);
         }
     });
 
     ws.on("close", () => {
         console.log("Client has disconnected");
-        // LED.writeSync(0);
     });
 
 });
