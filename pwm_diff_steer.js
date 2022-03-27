86476337960a3b7ed7014a48b8e371d0211b3b4a
niall james <niall.james@dvla.gov.uk>
2022-03-27 21:28:22 +0100

    test bed for differential steering

---
+++ pwm_diff_steer.js
@@ -0,0 +1,22 @@
+const Gpio = require('pigpio').Gpio;
+const left_pos = new Gpio(13, {mode: Gpio.OUTPUT});
+const left_neg = new Gpio(19, {mode: Gpio.OUTPUT});
+const right_pos = new Gpio(18, {mode: Gpio.OUTPUT});
+const right_neg = new Gpio(12, {mode: Gpio.OUTPUT});
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8081 });
+
+
+wss.on("connection", ws => {
+    console.log("New client connected");
+
+    ws.on("message", data => {
+        let input = `${data}`;
+        console.log(input)
+    });
+
+    ws.on("close", () => {
+        console.log("Client has disconnected");
+    });
+
+});
