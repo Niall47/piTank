86490a8bbc26b6bee9e36d14ac5d8f85e78060db
niall james <niall.james@dvla.gov.uk>
2022-03-30 20:12:52 +0100

    same again

--- pwm_diff_steer.js
+++ pwm_diff_steer.js
@@ -13,23 +13,23 @@ wss.on("connection", ws => {
 
     ws.on("message", data => {
         let input = `${data}`;
-        console.log(JSON.parse(input))
-        power = diffSteer(input.X, input.Y)
-        console.log(power)
-        if (parseInt(power[0]) < 0) {
-            left_pos.pwmWrite(power[0] * -1);
-            left_neg.pwmWrite(power[0] * -1);
-        } else {
-            left_pos.pwmWrite(power[0]);
-            left_neg.pwmWrite(0);
-        };
-        if (parseInt(power[1]) <= 0) {
-            right_pos.pwmWrite(power[1] * -1);
-            right_neg.pwmWrite(power[1] * -1);
-        } else {
-            right_pos.pwmWrite(power[1]);
-            right_neg.pwmWrite(0);
-        };
+        console.log(JSON.parse(input));
+        power = diffSteer(parseInt(input.X), parseInt(input.Y));
+        console.log(power);
+        // if (parseInt(power[0]) < 0) {
+        //     left_pos.pwmWrite(power[0] * -1);
+        //     left_neg.pwmWrite(power[0] * -1);
+        // } else {
+        //     left_pos.pwmWrite(power[0]);
+        //     left_neg.pwmWrite(0);
+        // };
+        // if (parseInt(power[1]) <= 0) {
+        //     right_pos.pwmWrite(power[1] * -1);
+        //     right_neg.pwmWrite(power[1] * -1);
+        // } else {
+        //     right_pos.pwmWrite(power[1]);
+        //     right_neg.pwmWrite(0);
+        // };
         // left_pos.pwmWrite(power[0]);
         // right_pos.pwmWrite(power[1]);
     });
