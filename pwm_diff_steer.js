8409d77fea00bda2d7d575a307b89b41ebcdeb7d
niall james <niall.james@dvla.gov.uk>
2022-03-30 20:02:09 +0100

    trying to work out turning. js debuggin sucks

--- pwm_diff_steer.js
+++ pwm_diff_steer.js
@@ -7,7 +7,7 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8081 });
 var diffSteer = require('diff-steer')
 
-
+// pigpio.initialize(); 
 wss.on("connection", ws => {
     console.log("New client connected");
 
@@ -15,8 +15,23 @@ wss.on("connection", ws => {
         let input = `${data}`;
         console.log(JSON.parse(input))
         power = diffSteer(input.X, input.Y)
-        left_pos.pwmWrite(power[0]);
-        right_pos.pwmWrite(power[1]);
+        console.log(power)
+        if (parseInt(power[0]) < 0) {
+            left_pos.pwmWrite(power[0] * -1);
+            left_neg.pwmWrite(power[0] * -1);
+        } else {
+            left_pos.pwmWrite(power[0]);
+            left_neg.pwmWrite(0);
+        };
+        if (parseInt(power[1]) <= 0) {
+            right_pos.pwmWrite(power[1] * -1);
+            right_neg.pwmWrite(power[1] * -1);
+        } else {
+            right_pos.pwmWrite(power[1]);
+            right_neg.pwmWrite(0);
+        };
+        // left_pos.pwmWrite(power[0]);
+        // right_pos.pwmWrite(power[1]);
     });
 
     ws.on("close", () => {
@@ -24,3 +39,12 @@ wss.on("connection", ws => {
     });
 
 });
+
+
+// process.on('SIGINT', () => {
+//     led.digitalWrite(0);
+//     pigpio.terminate(); // pigpio C library terminated here
+//     clearInterval(iv);
+//     console.log('Terminating...');
+//   });
+  
