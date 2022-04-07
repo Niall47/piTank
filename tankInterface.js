1e3568162ac582f599f8e45e4818cbe3f99f9121
niall james <niall.james@dvla.gov.uk>
2022-04-07 18:43:48 +0100

    Added drive conversion to joystick page

--- tankInterface.js
+++ tankInterface.js
@@ -1,16 +1,17 @@
 var joyParam = { "title": "joystick",
                 "width": 300,
                 "height": 300 };
-var Joy = new JoyStick('joyDiv', joyParam);
 var connectionStatus = false;
-refreshRate = document.getElementById("refreshRate");
-refreshRateText = document.getElementById("refreshRateText");
+let refreshRate = document.getElementById("refreshRate");
+let refreshRateText = document.getElementById("refreshRateText");
 connectionBlock = document.getElementById("connectionBlock");
-const status = document.getElementById("status"),
-direction = document.getElementById("direction"),
-customInput = document.getElementById("customIP"),
-connectButton = document.getElementById("connectButton")
+const status = document.getElementById("status");
+direction = document.getElementById("direction");
+customInput = document.getElementById("customIP");
+connectButton = document.getElementById("connectButton");
+driveValues = document.getElementById("driveValues");
 rateUpdate();
+Joy = new JoyStick('joyDiv', joyParam);
 
 function connect(t) {
 
@@ -36,16 +37,37 @@ function customConnect() {
 };
 
 function updateDisplay() {
-    1 == connectionStatus ? connectBlock.style.visibility = 'hidden' : connectBlock.style.visibility = 'visbible'
+    1 == connectionStatus ? connectionBlock.style.visibility = 'hidden' : connectionBlock.style.visibility = 'visbible'
+};
+
+function convert(x,y) {
+    r = Math.hypot(x,y);
+    t = Math.atan2(y,x);
+
+    t += Math.PI / 4;
+
+    left = r * Math.cos(t);
+    right = r * Math.sin(t);
+
+    left = left * Math.sqrt(2);
+    right = right * Math.sqrt(2);
+
+    left = Math.max(-1, Math.min(left, 1));
+    right = Math.max(-1, Math.min(right, 1));
+
+    return {left, right};
 };
 
 setInterval(function() {
-payload = {
-            X: Joy.GetX(),
-            Y: Joy.GetY()
-};
+    joyX = Joy.GetX() / 10;
+    joyY = Joy.GetY() / 10;
 
-direction.innerHTML = JSON.stringify(payload), 
-1 == connectionStatus && ws.send(JSON.stringify(payload));
-}, parseInt(refreshRate.value)), 
-connect("localhost"), updateDisplay();
+    payload = {X: joyX, Y: joyY};
+    // console.log(payload);
+    driveValues.innerHTML = JSON.stringify(convert(parseInt(joyX), parseInt(joyY)));
+    direction.innerHTML = JSON.stringify(payload), 
+    1 == connectionStatus && ws.send(JSON.stringify(payload));
+    }, parseInt(refreshRate.value)), 
+    connect("localhost"), updateDisplay();
+
+
