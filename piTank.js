5a36b14dcf74eca7d43dc99acfa119bb8429b22a
niall james <niall.james@dvla.gov.uk>
2022-12-11 19:17:32 +0000

    try converting to percentage

--- piTank.js
+++ piTank.js
@@ -23,24 +23,39 @@ wss.on("connection", ws => {
 
 });
 function driveMotors(left,right){
-    left = calculateInput(left);
-    right = calculateInput(right);
+
+    console.log(pwmValue(left) + '    '+ pwmValue(right))
+    // left = calculateInput(left);
+    // right = calculateInput(right);
 
     // left_pos.pwmWrite(left[0]);
     // left_neg.pwmWrite(left[1]);
     // right_pos.pw mWrite(right[0]);
     // right_neg.pwmWrite(right[1]);
 
-    console.log('Left: ' + left +  '   Right: ' + right);
+    // console.log('Left: ' + pwmValue(left) +  '   Right: ' + pwmValue(right));
 };
 
+function pwmValue(input){
+    positive = Math.round((input/100)*255);
+    if (input >= 0){
+        negative = 0;
+    } else {
+        negative = Math.round((input/100)*255);
+    }
+    negative = Math.round((input/100)*255);
+    return [positive, negative];
+}
 
-function calculateInput(percentage){
-    if (percentage === 0 ){
-        return [0,0]
-    } else if (percentage > 0){
-        console.log(percentage);
-    } else if (percentage < 0){
-        console.log(percentage)
-    };
-};
+
+// function calculateInput(percentage){
+//     if (percentage === 0 ){
+//         return [0,0]
+//     } else if (percentage > 0){
+//         return [pwmValue(percentage), pwmValue(percentage)]
+
+//         console.log(percentage);
+//     } else if (percentage < 0){
+//         console.log(percentage)
+//     };
+// };
