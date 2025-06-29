c1f30b0a54afb00f9f27e3b6342955f797760ff9
Niall James <niall.james@dvla.gov.uk>
2025-06-29 21:06:58 +0100

    remove unused

--- joystick_controller/socket.js
+++ joystick_controller/socket.js
@@ -18,68 +18,6 @@ function disconnect() {
     }
 };
 
-function connect(ip = '192.168.0.20', port = '8081') {
-    if (connectionStatus === 'Disconnected' || connectionStatus === 'Scanning') {
-        socket = new WebSocket(`ws://${ip}:${port}`);
-
-        socket.onopen = function() {
-            connectionStatus = 'Connected';
-            piTankIP = ip + ':' + port;
-            sendLog('Connected to ' + ip + ':' + port);
-            updateDisplay(connectionStatus);
-        };
-
-        socket.onclose = function() {
-            connectionStatus = 'Disconnected';
-            sendLog('Disconnected from ' + ip + ':' + port);
-            updateDisplay(connectionStatus);
-        };
-
-        socket.onerror = function(error) {
-            console.error('WebSocket error:', error);
-        };
-
-        socket.onmessage = function(event) {
-            console.log('Message from server:', event.data);
-            try {
-                const data = JSON.parse(event.data);
-                if (data.status === 'queued') {
-                    connectionStatus = 'Queued';
-                    sendLog('Queued—position: ' + data.position);
-                    updateDisplay(connectionStatus);
-                } else if (data.status === 'queue_update') {
-                    sendLog('Queue update—new position: ' + data.position); 
-                }else if (data.status === 'connected') {
-                    connectionStatus = 'Connected';
-                    sendLog('Server says connected');
-                    updateDisplay(connectionStatus);
-                } else if (data.status === 'disconnected') {
-                    connectionStatus = 'Disconnected';
-                    sendLog('Server disconnected'); 
-                    updateDisplay(connectionStatus);                
-                } else {
-                    console.log('Unknown JSON message from server:', event.data);
-                }
-            } catch (e) {
-                if (event.data === 'connected') {
-                    connectionStatus = 'Connected';
-                    console.log('Connected to ip:', data.ip, 'Port:', data.port);
-                    console.log('Status: ', event.data);
-                    updateDisplay(connectionStatus);
-                }
-                else if (event.data === 'disconnected') {
-                    connectionStatus = 'Disconnected';
-                    console.log('Disconnected from :', ip, 'Port:', port);
-                    updateDisplay(connectionStatus);
-                }
-                else {
-                    console.log('Unknown message from server:', event.data);
-                }
-            }
-        };
-    }
-
-};
 
 let scanning = false;
 let scanSockets = [];
@@ -116,8 +54,6 @@ function scan(targetIp = null, targetPort = null) {
         };
         ws.onerror = () => { ws.close(); reject(); };
         ws.onclose = () => { reject(); };
-        
-        // Add message handling from connect function
         ws.onmessage = function(event) {
             console.log('Message from server:', event.data);
             try {
@@ -159,21 +95,8 @@ function scan(targetIp = null, targetPort = null) {
     }));
 
     Promise.any(connectPromises)
-        .then(({ip, ws}) => {
-            sendLog('Found host at ' + ip);
-            scanning = false;
-            scanSockets.forEach(s => s !== ws && s.close());
-            scanSockets = [];
-            socket = ws;
-            connectionStatus = 'Connected';
-            customIPut.value = ip;
-            customPort.value = defaultPort;
-            sendLog('Connected to ' + ip + ':' + defaultPort);
-            updateDisplay(connectionStatus);
-        })
         .catch(() => {
             if (socket && socket.readyState === WebSocket.OPEN) {
-
                 connectionStatus = 'Connected';
                 sendLog('Connected to ' + piTankIP);
             } else {
